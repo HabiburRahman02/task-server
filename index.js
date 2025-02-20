aa07ffa185b49f0b3d379f5d82089c71f11aef9a
Habibur Rahman <habibur88454@gmail.com>
2025-02-20 17:48:14 +0600

    put method for task update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -40,16 +40,47 @@ async function run() {
     })
 
     // task related apis
-app.get('/tasks',async(req,res)=>{
-  const result  = await taskCollection.find().toArray();
-  res.send(result);
-})
+    app.delete('/deleteTaskById', async (req, res) => {
+      const id = req.params;
+      console.log(id);
+    })
+    app.get('/getTaskById', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await taskCollection.findOne(query);
+      res.send(result)
+    })
+    app.get('/tasks', async (req, res) => {
+      const result = await taskCollection.find().toArray();
+      res.send(result);
+    })
 
     app.post('/tasks', async (req, res) => {
       const task = req.body;
       const result = await taskCollection.insertOne(task);
       res.send(result);
     })
+
+    app.put('/tasks/:id', async (req, res) => {
+      const taskId = req.params.id;        
+      const { category } = req.body;         
+    
+      try {
+        const result = await taskCollection.updateOne(
+          { _id: new ObjectId(taskId) },    
+          { $set: { category: category } }   
+        );
+    
+        if (result.matchedCount === 0) {
+          return res.status(404).send({ message: "Task not found" });
+        }
+    
+        res.send({ message: "Task updated successfully", result });
+      } catch (error) {
+        console.error("Error updating task:", error);
+        res.status(500).send({ message: "Server error" });
+      }
+    });
 
 
 
